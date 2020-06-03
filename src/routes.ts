f04cc77e688e6cfd1dbc171f80b4da7e974b33b0
felipemartins8058 <felipecosmemartins10@gmail.com>
2020-06-03 13:17:37 -0300

    init

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import knex from './database/connection'
+
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/PointsController'
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.create);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+ export default routes;
